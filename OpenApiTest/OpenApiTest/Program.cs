e013f4de45192f93a689a94e7440c9571ac5cf94
Sam Xu <saxu@microsoft.com>
2017-11-22 11:32:16 -0800

    add a project to test the vsts feed

---
+++ OpenApiTest/OpenApiTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.OpenApi;
+using Microsoft.OpenApi.Extensions;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenApiTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OpenApiDocument document = new OpenApiDocument
+            {
+                SpecVersion = new Version(3, 0, 0)
+            };
+            Console.WriteLine(document.SerializeAsJson(OpenApiSpecVersion.OpenApi3_0_0));
+        }
+    }
+}
